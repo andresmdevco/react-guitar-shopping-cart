384d8eace6f97c91677fa450e61445b5dab11491
Andrés Muñoz <andres2078@gmail.com>
2026-06-19 13:49:01 -0500

    feat: render guitar components from state data

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
 
 	const [data, setData] = useState(db)
 
-
     return (
         <>
             <Header />
@@ -16,13 +15,9 @@ function App() {
                 <h2 className="text-center">Nuestra Colección</h2>
 
                 <div className="row mt-5">
-                    <Guitar />
-                    <Guitar />
-                    <Guitar />
-                    <Guitar />
-                    <Guitar />
-                    <Guitar />
-                    <Guitar />
+					{data.map(() => (
+						<Guitar />	
+					))}
                 </div>
             </main>
 
