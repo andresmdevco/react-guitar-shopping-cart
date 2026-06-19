572cdf6effd6e4abfa710036da9c9788c78f27ff
Andrés Muñoz <andres2078@gmail.com>
2026-06-19 13:25:21 -0500

    feat: add guitar catalog data and initialize state

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import Guitar from "./components/Guitar"
-import Header from "./components/Header"
-
+import { useState } from "react";
+import Guitar from "./components/Guitar";
+import Header from "./components/Header";
+import { db } from "./data/db";
 
 function App() {
 
-  return (
-    <>
-    <Header />
-
-    <main className="container-xl mt-5">
-        <h2 className="text-center">Nuestra Colección</h2>
-
-        <div className="row mt-5">
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
-        </div>
-    </main>
+	const [data, setData] = useState(db)
 
 
-    <footer className="bg-dark mt-5 py-5">
-        <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
-        </div>
-    </footer>
-    </>
-  )
+    return (
+        <>
+            <Header />
+
+            <main className="container-xl mt-5">
+                <h2 className="text-center">Nuestra Colección</h2>
+
+                <div className="row mt-5">
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                </div>
+            </main>
+
+            <footer className="bg-dark mt-5 py-5">
+                <div className="container-xl">
+                    <p className="text-white text-center fs-4 mt-4 m-md-0">
+                        GuitarLA - Todos los derechos Reservados
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
 }
 
-export default App
+export default App;
