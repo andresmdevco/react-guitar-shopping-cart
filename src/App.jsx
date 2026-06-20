d134fa1ae5b53763a6a607d19ba663457599f78d
Andrés Muñoz <andres2078@gmail.com>
2026-06-20 14:40:49 -0500

    feat: add click event handler to Guitar component

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,11 @@ function App() {
 
             <main className="container-xl mt-5">
                 <h2 className="text-center">Nuestra Colección</h2>
-
                 <div className="row mt-5">
 					{data.map((guitar) => (
 						<Guitar 
-							key = {guitar.id}
-							guitar = {guitar}
+							key={guitar.id}
+							guitar={guitar}
 						/>	
 					))}
                 </div>
