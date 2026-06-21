a213cc42972a751276935b63ca8fe45ee52977c4
Andrés Muñoz <andres2078@gmail.com>
2026-06-20 22:05:32 -0500

    feat: conditionally render cart contents

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,9 +21,9 @@ export default function Header({cart}) {
                             />
 
                             <div id="carrito" className="bg-white p-3">
-                                <p className="text-center">
-                                    El carrito esta vacio
-                                </p>
+                                {cart.length === 0 ? (
+                                    <p className="text-center">El carrito esta vacio</p>
+                                ) : (
                                 <table className="w-100 table">
                                     <thead>
                                         <tr>
@@ -73,6 +73,7 @@ export default function Header({cart}) {
                                         ))}
                                     </tbody>
                                 </table>
+                                )}
 
                                 <p className="text-end">
                                     Total pagar:{" "}
